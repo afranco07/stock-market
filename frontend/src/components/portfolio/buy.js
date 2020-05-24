a940ba42d810943794e5f618e82f5987d27539a4
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2020-05-24 19:21:11 -0400

    Frontend validations

--- frontend/src/components/portfolio/buy.js
+++ frontend/src/components/portfolio/buy.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import {add} from "../../features/stocks/stocksSlice"
-import {useDispatch} from "react-redux";
+import { add } from "../../features/stocks/stocksSlice"
+import { selectCash, setCash } from "../../features/user/userSlice";
+import {useDispatch, useSelector} from "react-redux";
 import Spinner from 'react-bootstrap/Spinner';
 
 export default function Buy() {
     const [ticker, setTicker] = useState("");
     const [amount, setAmount] = useState("");
     const [buying, setBuying] = useState(false);
+    const cash = useSelector(selectCash);
     const dispatch = useDispatch();
 
     const submitPurchase = (e) => {
@@ -27,20 +29,20 @@ export default function Buy() {
                 dispatch(add(stockData))
                 setTicker("");
                 setAmount("");
+                dispatch(setCash(cash - stockData.price));
                 setBuying(false);
             })
     };
     return(
         <Form onSubmit={submitPurchase} method="POST">
-            <h3>Cash - $5000.00</h3>
+            <h3>Cash - ${cash}</h3>
             <Form.Group controlId="ticker">
                 <Form.Control type="input" placeholder="Ticker" value={ticker} onChange={(e) => setTicker(e.target.value)}/>
             </Form.Group>
-
             <Form.Group controlId="qty">
-                <Form.Control type="number" placeholder="Qty" value={amount} onChange={(e) => setAmount(e.target.value)} />
+                <Form.Control type="number" min="1" placeholder="Qty" value={amount} onChange={(e) => setAmount(e.target.value)} />
             </Form.Group>
-            <Button variant="primary" block type="submit" disabled={buying}>
+            <Button variant="primary" block type="submit" disabled={buying || ticker.length < 1}>
                 {buying ? <Spinner animation="border" size="sm" role="status" as="span"/> : "Submit"}
             </Button>
         </Form>
